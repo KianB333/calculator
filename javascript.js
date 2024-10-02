67316938a741203451588d424a2b65fcdbe2bb35
Kian <Kian.berg333@gmail.com>
2024-10-01 18:02:51 -0600

    Update display on-click

--- javascript.js
+++ javascript.js
@@ -1,3 +1,37 @@
+const displayValue = document.querySelector(".display");
+displayValue.textContent = "";
+
+const btn1 = document.querySelector(".one");
+btn1.addEventListener("click", (e) => displayValue.textContent += `${e.target.textContent + " "}`)
+const btn2 = document.querySelector(".two");
+btn2.addEventListener("click", (e) => displayValue.textContent += `${e.target.textContent + " "}`)
+const btn3 = document.querySelector(".three");
+btn3.addEventListener("click", (e) => displayValue.textContent += `${e.target.textContent + " "}`)
+const btn4 = document.querySelector(".four");
+btn4.addEventListener("click", (e) => displayValue.textContent += `${e.target.textContent + " "}`)
+const btn5 = document.querySelector(".five");
+btn5.addEventListener("click", (e) => displayValue.textContent += `${e.target.textContent + " "}`)
+const btn6 = document.querySelector(".six");
+btn6.addEventListener("click", (e) => displayValue.textContent += `${e.target.textContent + " "}`)
+const btn7 = document.querySelector(".seven");
+btn7.addEventListener("click", (e) => displayValue.textContent += `${e.target.textContent + " "}`)
+const btn8 = document.querySelector(".eight");
+btn8.addEventListener("click", (e) => displayValue.textContent += `${e.target.textContent + " "}`)
+const btn9 = document.querySelector(".nine");
+btn9.addEventListener("click", (e) => displayValue.textContent += `${e.target.textContent + " "}`)
+const btn0 = document.querySelector(".zero");
+btn0.addEventListener("click", (e) => displayValue.textContent += `${e.target.textContent + " "}`)
+const btnAdd = document.querySelector(".add");
+btnAdd.addEventListener("click", (e) => displayValue.textContent += `${e.target.textContent + " "}`)
+const btnSub = document.querySelector(".subtract");
+btnSub.addEventListener("click", (e) => displayValue.textContent += `${e.target.textContent + " "}`)
+const btnMult = document.querySelector(".multiply");
+btnMult.addEventListener("click", (e) => displayValue.textContent += `${e.target.textContent + " "}`)
+const btnDiv = document.querySelector(".divide");
+btnDiv.addEventListener("click", (e) => displayValue.textContent += `${e.target.textContent + " "}`)
+//Adds button content to display on click.
+
+
 let num1;
 let op;
 let num2;
