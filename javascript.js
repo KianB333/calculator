d84d012f26d58c9e9a86023e4363288dd8f72a25
Kian <Kian.berg333@gmail.com>
2024-11-02 23:45:18 -0600

    Make display update instead of falsely adding nums
    Display AND calculations are taking num1 and num2 in reverse order...

--- javascript.js
+++ javascript.js
@@ -6,41 +6,26 @@ let op = "";
 let num2 = "";
 let results = "";
 
-function addNumber(num) {
+/*function addNumber(num) {
     if(op) {
         num2 += num;
     } else {
         num1 += num;
     }
-};
+};*/
+//Amazing how similar my ideas were before taking a 1 month break, shown ^above vs ⌄below
+
+function addNum(numValue) {
+    op ? num1 += numValue : num2 += numValue;
+
+}
+//Updates num1/num2, based on whether operator exists yet.
+
 
 function addOperator(operator) {
     op = operator;
 }
-
-function operate(num1, op, num2) {
-    switch (op) {
-        case "add":
-            results = Number(num1) + Number(num2);
-            break;
-
-        case "subtract":
-            results = Number(num1) - Number(num2);
-            break;
-
-        case "multiply":
-            results = Number(num1) * Number(num2);
-            break;
-
-        case "divide":
-            results = Number(num1) / Number(num2); 
-            break;
-
-        default:
-            results = "ERROR";
-            console.log("Something went wrong with operate!");
-    }
-}
+//Updates operator for calculation
 
 function clearCalc() {
     num1 = "";
@@ -49,87 +34,113 @@ function clearCalc() {
     displayValue.textContent = "";
     allBtns.forEach((btn) => btn.removeEventListener("mouseup", clearCalc));
 }
-//Clears all values + removes the clearCalc() event listeners
+//Clears all values + removes the event listeners triggering clearCalc()
+
+function operate(num1, op, num2) {
+    switch (op) {
+        case "+":
+            results = Number(num1) + Number(num2);
+            break;
+
+        case "-":
+            results = Number(num1) - Number(num2);
+            break;
+
+        case "*":
+            results = Number(num1) * Number(num2);
+            break;
+
+        case "÷":
+            results = Number(num1) / Number(num2); 
+            break;
+
+        default:
+            results = "ERROR";
+            console.log("Something went wrong with operate!");
+    }
+}
+//The actual calculation
 
 const allBtns = document.querySelectorAll("button:not(.equals, .delete, .clear)");
-//For all numeric buttons
+//Lumps all numeric buttons for clearCalc()
 
 const btn1 = document.querySelector(".one");
 btn1.addEventListener("click", (e) => {
-    displayValue.textContent += `${e.target.textContent}`;
-    addNumber("1");
+    addNum("1");
+    updateDisplay();
 });
 const btn2 = document.querySelector(".two");
 btn2.addEventListener("click", (e) => {
-    displayValue.textContent += `${e.target.textContent}`;
-    addNumber("2");
+    addNum("2");
+    updateDisplay();
 });
 const btn3 = document.querySelector(".three");
 btn3.addEventListener("click", (e) => {
-    displayValue.textContent += `${e.target.textContent}`;
-    addNumber("3");
+    addNum("3");
+    updateDisplay();
 });
 const btn4 = document.querySelector(".four");
 btn4.addEventListener("click", (e) => {
-    displayValue.textContent += `${e.target.textContent}`;
-    addNumber("4");
+    addNum("4");
+    updateDisplay();
 });
 const btn5 = document.querySelector(".five");
 btn5.addEventListener("click", (e) => {
-    displayValue.textContent += `${e.target.textContent}`;
-    addNumber("5");
+    addNum("5");
+    updateDisplay();
 });
 const btn6 = document.querySelector(".six");
 btn6.addEventListener("click", (e) => {
-    displayValue.textContent += `${e.target.textContent}`;
-    addNumber("6");
+    addNum("6");
+    updateDisplay();
 });
 const btn7 = document.querySelector(".seven");
 btn7.addEventListener("click", (e) => {
-    displayValue.textContent += `${e.target.textContent}`;
-    addNumber("7");
+    addNum("7");
+    updateDisplay();
 });
 const btn8 = document.querySelector(".eight");
 btn8.addEventListener("click", (e) => {
-    displayValue.textContent += `${e.target.textContent}`;
-    addNumber("8");
+    addNum("8");
+    updateDisplay();
 });
 const btn9 = document.querySelector(".nine");
 btn9.addEventListener("click", (e) => {
-    displayValue.textContent += `${e.target.textContent}`;
-    addNumber("9");
+    addNum("9");
+    updateDisplay();
 });
 const btn0 = document.querySelector(".zero");
 btn0.addEventListener("click", (e) => {
-    displayValue.textContent += `${e.target.textContent}`;
-    addNumber("0");
+    addNum("0");
+    updateDisplay();
 });
 const btnDot = document.querySelector(".dot");
 btnDot.addEventListener("click", (e) => {
-    displayValue.textContent += `${e.target.textContent}`;
-    addNumber(".");
+    addNum(".");
+    updateDisplay();
 });
 const btnAdd = document.querySelector(".add");
 btnAdd.addEventListener("click", (e) => {
-    displayValue.textContent += `${" " + e.target.textContent + " "}`;
-    addOperator("add");
+    addOperator("+");
+    updateDisplay();
 });
 const btnSub = document.querySelector(".subtract");
 btnSub.addEventListener("click", (e) => {
-    displayValue.textContent += `${" " + e.target.textContent + " "}`;
-    addOperator("subtract");
+    addOperator("-");
+    updateDisplay();
 });
 const btnMult = document.querySelector(".multiply");
 btnMult.addEventListener("click", (e) => {
-    displayValue.textContent += `${" " + e.target.textContent + " "}`;
-    addOperator("multiply");
+    addOperator("*");
+    updateDisplay();
 });
 const btnDiv = document.querySelector(".divide");
 btnDiv.addEventListener("click", (e) => {
-    displayValue.textContent += `${" " + e.target.textContent + " "}`;
-    addOperator("divide");
+    addOperator("÷");
+    updateDisplay();
 });
 //Updates num1/op/num2 values, and adds button values to display
+
 
 const btnEqual = document.querySelector(".equals");
 btnEqual.addEventListener("click", () => {
@@ -141,4 +152,8 @@ btnEqual.addEventListener("click", () => {
 
 const btnClear = document.querySelector(".clear");
 btnClear.addEventListener("click", () => clearCalc())
-//Clears all values
+//Clears all values when clicked
+
+function updateDisplay() {
+    displayValue.textContent = `${num1} ${op} ${num2}`;
+}
